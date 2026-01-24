4f8e8deee6f47adc3756e438e56657dc7e2a65f4
836-code <liuwenqi1046127836@gmail.com>
2026-01-24 23:29:34 +0800

    Delete vue3 directory

--- vue3/src/types/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentCustomProperties } from "vue";
-import request from "@/utils/request";
-
-declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    request: typeof request;
-  }
-}
