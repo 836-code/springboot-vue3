56c80000c2657d8d7406ff61f175473776b028b8
心靈感悟 <xinling@example.com>
2026-01-30 19:06:15 +0800

    添加springboot图片去背景功能。

--- src/main/java/com/gpan/springboot/SpringbootApplication.java
+++ src/main/java/com/gpan/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package com.gpan.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gpan")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
